640f73063dae5ea756e544189bb8cf619349f8b3
Umang Goel <umanggoel27@gmail.com>
2023-07-26 16:57:21 +0530

    initial commit

--- src/app/about.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const about = () => {
-  return <div>Hello about how are you</div>;
-};
--- src/app/index.js
+++
@@ -1,9 +0,0 @@
-function handleSubmit () {
-    const fname = document.getElementById('fname').value;
-    const age = document.getElementById('age').value;
-
-    localStorage.setItem("NAME", fname);
-    localStorage.setItem("AGE ",age);
-
-    return;
-}
